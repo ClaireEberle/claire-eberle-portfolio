617739a153f2a41761b6cd292ae2bbf04b1c5499
Claire <claire.3.eberle@gmail.com>
2023-03-12 14:42:34 -0700

    added changes to portfolio cards

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
+const styleCard = {
+width: "18rem",
+margin: "10px"
+}
 export const Cards = (props) => {
-  return (
-    <div>
-        <div className='card'>
-            <img src={props.img} className="card-img" alt="Project Image"/>
-            <div className='card-body'>
-            <h3>{props.title}</h3>
-            <p className='card-text'>{props.description}</p>
+
+    return (
+      <div>
+        <div className='card' style={styleCard}>
+            <img src={props.image} className="card-img-top" alt="Project Image"/>
+            <div className='card-body d-flex flex-column'>
+            <h3 className='card-title'>{props.title}</h3>
+            <p className='card-text'>{props.description}</p> 
+            <a href={props.github} class="card-link">GitHub</a>
+    <a href={props.site} class="card-link">Site Link</a>
             
             </div>
         </div>
--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Cards } from "../../Cards"
 
 export const Portfolio = () => {
+
+  const [projects, setProjects] = useState([
+    {
+      "image": "./stream.png",
+      "title":"Streamasaurus",
+      "description":"This project is d***",
+      "linkGithub": "/",
+      "linkSite":"/"
+    },
+    {
+      "image": "./saveSeat.png",
+      "title":"Save Your Seat",
+      "description":"This project is nice",
+      "linkGithub": "https://github.com/ClaireEberle/Save-Your-Seat",
+      "linkSite":"https://make-reservation.herokuapp.com/"
+    },
+  ])
   return (
     <div>
       <h1>Portfolio</h1>
-      <Cards/>
-      <Cards/>
-      <Cards/>
-      <Cards/>
+      {projects.map(project=>
+      <Cards image={project.image} description={project.description} github={project.linkGithub} site={project.linkSite}/>
+        )}
     </div> 
   )
 }

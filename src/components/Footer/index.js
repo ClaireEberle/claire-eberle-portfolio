bc1de787d65adeff2b9de5399c9a5c6a73f6e6ed
Claire <claire.3.eberle@gmail.com>
2024-02-29 09:44:26 -0800

    changed formatting

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
 export const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='footer-nav'>
-
-        <ul className='footer-ul'>
-            <li className='footer-li'>
-               <a className='footer-a' href="https://github.com/ClaireEberle">Github</a> 
-            </li>
-            <li className='footer-li'>
-               <a className='footer-a' href="https://www.linkedin.com/in/claire-eberle/">Linkedin</a> 
-            </li>
-            
+    <div className="footer">
+      <div className="footer-nav">
+        <ul className="footer-ul">
+          <li className="footer-li">
+            <a className="footer-a" href="https://github.com/ClaireEberle">
+              Github
+            </a>
+          </li>
+          <li className="footer-li">
+            <a
+              className="footer-a"
+              href="https://www.linkedin.com/in/claire-eberle/"
+            >
+              Linkedin
+            </a>
+          </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const cors = require('cors');
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
@@ -8,45 +8,44 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", //replace with your email provider
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS
-    }
-  });
-  transporter.verify(function(error, success) {
-    if (error) {
-      console.log(error);
+  host: "smtp.gmail.com", //replace with your email provider
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+app.post("/send", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  const mail = {
+    from: name,
+    to: "claireeberle@898gmail.com",
+    subject: email,
+    text: message,
+  };
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
     } else {
-      console.log("Server is ready to take our messages");
+      res.json({
+        status: "success",
+      });
     }
   });
-  app.post('/send', (req,res)=>{
-    const name = req.body.name
-    const email = req.body.email
-    const message = req.body.message
-    
-    const mail = {
-        from: name, 
-        to: "claireeberle@898gmail.com",
-        subject: email,
-        text: message
-    }
-    transporter.sendMail(mail, (err, data) => {
-        if (err) {
-          res.json({
-            status: 'fail'
-          })
-        } else {
-          res.json({
-           status: 'success'
-          })
-        }
-      })
-  })
+});
